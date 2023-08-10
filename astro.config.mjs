d9382f16396879fb54df834bda1b8161f6acc0fe
Kushajveer Singh <kushajreal@gmail.com>
2023-08-10 16:23:39 -0400

    Make currently working on hidden and rename it to "todo"

--- astro.config.mjs
+++ astro.config.mjs
@@ -42,7 +42,6 @@ export default defineConfig({
             ]},
           ],
         },
-        { label: 'Currently working on', link: '/currently_working_on' },
         {
           label: 'Header Links',
           collapsed: true,

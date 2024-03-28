3ae14276032c73dcf58d0cb155025e76cb095c94
Kushajveer Singh <kushajreal@gmail.com>
2024-03-28 02:22:51 -0400

    setup 1

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Layout from '@theme/Layout';
+
+export default function Home(): JSX.Element {
+  return (
+    <Layout
+      title={`Kushajveer Singh portfolio page`}
+      description='Full stack software engineer'
+    >
+      Migrating my site to over here.
+    </Layout>
+  );
+}

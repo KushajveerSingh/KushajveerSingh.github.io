15e3cd17b34e82f08e1d8c9eb5183bd3a8d79f35
Kushajveer Singh <kushajreal@gmail.com>
2024-04-05 02:47:06 -0400

    setup complete

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,8 +2,11 @@ import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
-const remarkPlugins: any[] = [];
-const rehypePlugins: any[] = [];
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
+const remarkPlugins: any[] = [remarkMath];
+const rehypePlugins: any[] = [rehypeKatex];
 
 const docs_obj = {
   editUrl:
@@ -112,6 +115,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.palenight,
       darkTheme: prismThemes.palenight,
+      additionalLanguages: [],
     },
 
     footer: {
@@ -347,6 +351,16 @@ const config: Config = {
   },
 
   headTags: [],
+
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
 
   scripts: [
     {

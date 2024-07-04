4183a4607dd7bfdd377c151cb3b1f4257b7429a7
Kushajveer Singh <kushajreal@gmail.com>
2024-07-04 18:01:26 -0400

    change config file, remove blog plugin, remove math plugins, remove beam script

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,11 +2,8 @@ import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
-
-const remarkPlugins: any[] = [remarkMath];
-const rehypePlugins: any[] = [rehypeKatex];
+const remarkPlugins: any[] = [];
+const rehypePlugins: any[] = [];
 
 const docs_obj = {
   editUrl:
@@ -18,7 +15,7 @@ const docs_obj = {
 };
 
 const config: Config = {
-  title: 'Kushajveer Singh',
+  title: 'Kushajveer Singh Sooch',
   url: 'https://kushajveersingh.com/',
   baseUrl: '/',
 
@@ -35,7 +32,7 @@ const config: Config = {
   onDuplicateRoutes: 'throw',
 
   tagline:
-    'Full Stack Software Engineer with 7 years of experience in architecting and deploying end-to-end software solutions for internal tools and dashboards, with a focus on creating fail -safe systems optimized for performance and memory efficiency.',
+    'Full-stack software engineer. Love hiking & orienteering, painting in various mediums, and occasionally crafting origami.',
 
   organizationName: 'KushajveerSingh',
   projectName: 'KushajveerSingh.github.io',
@@ -48,12 +45,18 @@ const config: Config = {
 
     image: 'profile.jpeg',
 
-    metadata: [],
+    metadata: [
+      {
+        name: 'description',
+        content:
+          'Full-stack software engineer. Love hiking & orienteering, painting in various mediums, and occasionally crafting origami.',
+      },
+    ],
 
     navbar: {
-      title: 'Kushajveer Singh',
+      title: 'Kushajveer Singh Sooch',
       logo: {
-        alt: 'Kushajveer Singh profile picture',
+        alt: 'Kushajveer Singh Sooch profile picture',
         src: 'profile.jpeg',
       },
       items: [
@@ -77,7 +80,7 @@ const config: Config = {
           position: 'left',
           label: 'Certifications',
         },
-        { to: '/blog', label: 'Blog', position: 'left' },
+        // { to: '/blog', label: 'Blog', position: 'left' },
         {
           type: 'dropdown',
           label: 'Socials',
@@ -88,16 +91,12 @@ const config: Config = {
               href: 'https://github.com/KushajveerSingh',
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/Kkushaj',
-            },
-            {
-              label: 'LinkedIn',
-              href: 'https://www.linkedin.com/in/kushaj/',
-            },
-            {
               label: 'Instagram',
               href: 'https://www.instagram.com/kushajveersingh/',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/Kkushaj',
             },
           ],
         },
@@ -119,7 +118,7 @@ const config: Config = {
     },
 
     footer: {
-      copyright: `Copyright © ${new Date().getFullYear()} Kushajveer Singh. All rights reserved.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Kushajveer Singh Sooch. All rights reserved.`,
       style: 'dark',
       links: [
         {
@@ -177,68 +176,68 @@ const config: Config = {
             },
           ],
         },
-        {
-          title: 'Blog',
-          items: [
-            {
-              label: 'Blog',
-              href: '/blog',
-            },
-            {
-              html: `<div style="padding-bottom: 10px;"></div>`,
-            },
-            {
-              html: `
-              <a
-                href="https://kushajveersingh.com/blog/tags"
-                class="footer__link-item"
-              >
-                Tags
-              </a>
-              `,
-            },
-            {
-              html: `
-              <a
-                href="https://kushajveersingh.com/blog/archive"
-                class="footer__link-item"
-              >
-                Archive
-              </a>
-              `,
-            },
-            {
-              html: `
-              <a
-                href="https://kushajveersingh.com/blog/rss.xml"
-                class="footer__link-item"
-              >
-                RSS.xml
-              </a>
-              `,
-            },
-            {
-              html: `
-              <a
-                href="https://kushajveersingh.com/blog/atom.xml"
-                class="footer__link-item"
-              >
-                Atom.xml
-              </a>
-              `,
-            },
-            {
-              html: `
-              <a
-                href="https://kushajveersingh.com/blog/feed.json"
-                class="footer__link-item"
-              >
-                Feed.json
-              </a>
-              `,
-            },
-          ],
-        },
+        // {
+        //   title: 'Blog',
+        //   items: [
+        //     {
+        //       label: 'Blog',
+        //       href: '/blog',
+        //     },
+        //     {
+        //       html: `<div style="padding-bottom: 10px;"></div>`,
+        //     },
+        //     {
+        //       html: `
+        //       <a
+        //         href="https://kushajveersingh.com/blog/tags"
+        //         class="footer__link-item"
+        //       >
+        //         Tags
+        //       </a>
+        //       `,
+        //     },
+        //     {
+        //       html: `
+        //       <a
+        //         href="https://kushajveersingh.com/blog/archive"
+        //         class="footer__link-item"
+        //       >
+        //         Archive
+        //       </a>
+        //       `,
+        //     },
+        //     {
+        //       html: `
+        //       <a
+        //         href="https://kushajveersingh.com/blog/rss.xml"
+        //         class="footer__link-item"
+        //       >
+        //         RSS.xml
+        //       </a>
+        //       `,
+        //     },
+        //     {
+        //       html: `
+        //       <a
+        //         href="https://kushajveersingh.com/blog/atom.xml"
+        //         class="footer__link-item"
+        //       >
+        //         Atom.xml
+        //       </a>
+        //       `,
+        //     },
+        //     {
+        //       html: `
+        //       <a
+        //         href="https://kushajveersingh.com/blog/feed.json"
+        //         class="footer__link-item"
+        //       >
+        //         Feed.json
+        //       </a>
+        //       `,
+        //     },
+        //   ],
+        // },
         {
           title: 'Socials',
           items: [
@@ -247,16 +246,12 @@ const config: Config = {
               href: 'https://github.com/KushajveerSingh',
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/Kkushaj',
-            },
-            {
-              label: 'LinkedIn',
-              href: 'https://www.linkedin.com/in/kushaj/',
-            },
-            {
               label: 'Instagram',
               href: 'https://www.instagram.com/kushajveersingh/',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/Kkushaj',
             },
           ],
         },
@@ -279,27 +274,27 @@ const config: Config = {
           routeBasePath: 'notes',
           sidebarPath: './docs/sidebars/notes.ts',
         },
-        blog: {
-          editUrl:
-            'https://github.com/KushajveerSingh/KushajveerSingh.github.io/edit/main/',
-          blogTitle: 'Kushajveer Singh blog',
-          blogDescription: 'Blog',
-          blogSidebarCount: 'ALL',
-          blogSidebarTitle: 'All posts',
-          postsPerPage: 'ALL',
-          remarkPlugins: remarkPlugins,
-          rehypePlugins: rehypePlugins,
-          showReadingTime: true,
-          feedOptions: {
-            type: 'all',
-            limit: false,
-            title: 'Kushajveer Singh Blog',
-            description: 'Blog',
-            copyright: `Copyright © ${new Date().getFullYear()} Kushajveer Singh Blog. All rights reserved.`,
-            language: 'en-US',
-          },
-          showLastUpdateTime: true,
-        },
+        // blog: {
+        //   editUrl:
+        //     'https://github.com/KushajveerSingh/KushajveerSingh.github.io/edit/main/',
+        //   blogTitle: 'Kushajveer Singh blog',
+        //   blogDescription: 'Blog',
+        //   blogSidebarCount: 'ALL',
+        //   blogSidebarTitle: 'All posts',
+        //   postsPerPage: 'ALL',
+        //   remarkPlugins: remarkPlugins,
+        //   rehypePlugins: rehypePlugins,
+        //   showReadingTime: true,
+        //   feedOptions: {
+        //     type: 'all',
+        //     limit: false,
+        //     title: 'Kushajveer Singh Blog',
+        //     description: 'Blog',
+        //     copyright: `Copyright © ${new Date().getFullYear()} Kushajveer Singh Sooch Blog. All rights reserved.`,
+        //     language: 'en-US',
+        //   },
+        //   showLastUpdateTime: true,
+        // },
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -362,23 +357,9 @@ const config: Config = {
 
   headTags: [],
 
-  stylesheets: [
-    {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
-      integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
-    },
-  ],
+  stylesheets: [],
 
-  scripts: [
-    {
-      src: 'https://beamanalytics.b-cdn.net/beam.min.js',
-      'data-token': '7b93b436-c2e2-4be3-b042-8e41b6afdaf6',
-      async: true,
-    },
-  ],
+  scripts: [],
 
   themes: ['@docusaurus/theme-mermaid'],
 };

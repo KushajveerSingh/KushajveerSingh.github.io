5fcd4319a2c44c23b141c83883cdd6bb4b569aea
Kushajveer Singh <kushajreal@gmail.com>
2024-07-10 16:04:20 -0400

    add aditya verma hea

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,8 +2,11 @@ import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
-const remarkPlugins: any[] = [];
-const rehypePlugins: any[] = [];
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
+const remarkPlugins: any[] = [remarkMath];
+const rehypePlugins: any[] = [rehypeKatex];
 
 const docs_obj = {
   editUrl:
@@ -357,7 +360,15 @@ const config: Config = {
 
   headTags: [],
 
-  stylesheets: [],
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
 
   scripts: [],
 
